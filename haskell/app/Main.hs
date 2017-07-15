1a06dea722deaeb9867fdcec811f1cc65be43721
Arun Raghavan <arun@arunraghavan.net>
2017-07-15 10:50:47 +0530

    haskell: Fix up for conciseness
    Based on comments from @garetxe who maintains the haskell-gi bindings,
    this does make the example more concise.
    
    Fixes include:
    
      - Drop commented sections that come from template stack.yaml
      - Drop extra-deps in stack.yaml since gi-* packages are in stackage
      - Make Data.Text import qualified
      - Use constructor properties for as much stuff as we can
      - Don't need to explicitly provide application flags if none

--- haskell/app/Main.hs
+++ haskell/app/Main.hs
@@ -5,33 +5,33 @@ import qualified GI.Gio as Gio
 import qualified GI.Gtk as Gtk
 import Control.Monad (void)
 import Data.GI.Base
-import Data.Text
+import qualified Data.Text as T (pack)
 import System.Environment (getArgs)
 
 main :: IO ()
 main = do
-  app <- Gtk.applicationNew (Just "com.example.hello") [Gio.ApplicationFlagsFlagsNone]
+  app <- Gtk.applicationNew (Just "com.example.hello") []
 
   Gio.onApplicationActivate app (onActivate app)
 
   args <- getArgs
-  void $ #run app (Just $ pack <$> args)
+  void $ #run app (Just $ T.pack <$> args)
 
   where
     onActivate :: Gtk.Application -> IO ()
     onActivate app = do
-      win <- new Gtk.Window [ #type := Gtk.WindowTypeToplevel ]
-      #setDefaultSize win 600 300
+      win <- new Gtk.Window [ #type := Gtk.WindowTypeToplevel
+                            , #defaultWidth := 600
+                            , #defaultHeight := 300 ]
       #addWindow app win
       
-      headerBar <- Gtk.headerBarNew
-      #setTitle headerBar (Just "Hello, World!")
-      #setShowCloseButton headerBar True
+      headerBar <- new Gtk.HeaderBar [ #title :=  "Hello, World!"
+                                     , #showCloseButton := True ]
       #show headerBar
       #setTitlebar win (Just headerBar)
 
-      label <- new Gtk.Label [ #label := "<span weight=\"bold\" size=\"larger\">Hello, World!</span>" ]
-      #setUseMarkup label True
+      label <- new Gtk.Label [ #label := "<span weight=\"bold\" size=\"larger\">Hello, World!</span>"
+                             , #useMarkup := True ]
       #show label
       #add win label
 
